828ca12d0b0ba3e031bca2517537985cd25ebf22
kaybow <k.bowmaster@hotmail.com>
2016-03-02 14:44:40 -0500

    Broke my code. Troubleshooting.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-var letter = prompt('Enter a letter');
+var letter = prompt('Enter a letter.');
+var i;
+var i1;
 
 for (i = 0; i < 11; i++) {
-	for <i1 = 0; counter1 <= counter; counter1++) {
-  document.write(letter + '<br>');
-}
+  for <i1 = 0; i1 <= i; i1++) {
+    document.write(letter + '<br>');
+  }
